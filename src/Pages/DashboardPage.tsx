850710931f56882de18596b8340c6522a5231601
vesko972 <dust9722@gmail.com>
2024-09-01 19:35:52 +0300

    test

--- src/Pages/DashboardPage.tsx
+++ src/Pages/DashboardPage.tsx
@@ -1,7 +1,25 @@
 import classes from './PagesStyle.module.css'
-
+import appConfig from '../../appConfig.json';
+import { useEffect } from 'react'
 const DashboardPage = () => {
+  console.log(sessionStorage)
+  const getData = async() => {
+  const url = appConfig.environment[appConfig.environment.env as 'LOCAL' | 'PROD'].url;
+    await fetch(`${url}/orders`,{
+      method:"get",
+      headers:{
+        'Content-Type':'application/json',
+        'Authorization':sessionStorage.authToken
+      }
+    })
+    .then(res => res.json())
+    .then(result => console.log(result))
+  }
 
+
+  useEffect(()=>{
+    getData()
+  },[])
   return (
     <div className={classes.mainContentWrapper}>
       <p>DASHBOARD PAGE !!</p>
